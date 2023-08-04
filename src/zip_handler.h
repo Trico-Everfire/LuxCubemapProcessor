e1b3bbde275f054e339afd75e48fc2c176b15470
Trico-Everfire <bobbys@live.nl>
2023-08-04 02:34:13 +0200

    chore: library clutter

--- src/zip_handler.h
+++ src/zip_handler.h
@@ -4,11 +4,6 @@
 #include <vector>
 extern "C" {
 #include "mz_compat.h"
-#include "mz_zip.h"
-#include "mz_strm.h"
-#include "mz_zip_rw.h"
-#include "mz_strm_os.h"
-#include "mz_os.h"
 }
 #include <unordered_map>
 
